94063412e8cf9e36d4157846861861b6cce9f8a1
Md. Rashed Miah <rashed.bcse.edu@gmail.com>
2024-09-19 14:52:11 +0600

    update registration

---
+++ src/app/auth/registration/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BackToHome } from '../login/components';
+import RegistrationForm from '@/components/auth/RegistrationForm';
+import Image from 'next/image';
+
+const Page = () => {
+    return (
+        <div className="bg-gradient-to-r from-pink-200 to-blue-200 min-h-screen w-full">
+            <div className="container mx-auto px-4">
+                <div className="lg:flex items-center justify-center gap-10 pt-16">
+                    <div className="hidden lg:block">
+                        {/* BackToHome component */}
+                        <BackToHome />
+                        
+                        {/* Registration Background Image */}
+                        <div className="relative">
+                            <Image
+                                src="/auth/register_bg.png"
+                                width={500}
+                                height={500}
+                                alt="Registration background image"
+                                className="object-contain"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Registration Form Section */}
+                    <div className="w-full lg:w-[50%]">
+                        <RegistrationForm />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
